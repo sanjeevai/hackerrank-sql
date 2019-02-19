90d8ff8d8e998bc98d2f66eb7f602a20c67c99ee
Sanjeev Yadav <sanjeev.yadav.ai@outlook.com>
2019-02-20 00:28:40 +0530

    feat: Add select-query-i

---
+++ basic-select/revising-the-select-query-i.sql
@@ -0,0 +1,10 @@
+-- Author: Sanjeev Yadav
+-- Created: 20 Feb 2019
+-- Domain: SQL
+-- Problem: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+-- Subdomain: Basic Select
+-- Title: Revising the Select Query I
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
