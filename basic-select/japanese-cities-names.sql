a27ffa700115b8c89777c8785cb10cd46ea1a0c6
Sanjeev Yadav <sanjeev.yadav.ai@outlook.com>
2019-02-20 00:28:25 +0530

    feat: Add japanese cities names

---
+++ basic-select/japanese-cities-names.sql
@@ -0,0 +1,10 @@
+-- Author: Sanjeev Yadav
+-- Created: 20 Feb 2019
+-- Domain: SQL
+-- Problem: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+-- Subdomain: Basic Select
+-- Title: Japanese Cities' Names
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
