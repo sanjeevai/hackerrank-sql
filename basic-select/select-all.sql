d22765f2a433b88509e8347d0ac93408b31a1a45
Sanjeev Yadav <sanjeev.yadav.ai@outlook.com>
2019-02-20 00:28:52 +0530

    feat: Add select-all

---
+++ basic-select/select-all.sql
@@ -0,0 +1,9 @@
+-- Author: Sanjeev Yadav
+-- Created: 20 Feb 2019
+-- Domain: SQL
+-- Problem: https://www.hackerrank.com/challenges/select-all-sql/problem
+-- Subdomain: Basic Select
+-- Title: Select All
+
+SELECT *
+FROM CITY
