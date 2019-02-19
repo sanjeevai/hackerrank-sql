7afcd13d9ac6ee0998e806835837df695933a29c
Sanjeev Yadav <sanjeev.yadav.ai@outlook.com>
2019-02-20 00:28:10 +0530

    feat: Add japanese cities attributes

---
+++ basic-select/japanese-cities-attributes.sql
@@ -0,0 +1,10 @@
+-- Author: Sanjeev Yadav
+-- Created: 20 Feb 2019
+-- Domain: SQL
+-- Problem: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+-- Subdomain: Basic Select
+-- Title: Japanese Cities' Attributes
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
