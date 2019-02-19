7b7780b4a31d24a1b3d07882fc1316428fc48ab2
Sanjeev Yadav <sanjeev.yadav.ai@outlook.com>
2019-02-20 00:29:00 +0530

    feat: Add select-by-id

---
+++ basic-select/select-by-id.sql
@@ -0,0 +1,10 @@
+-- Author: Sanjeev Yadav
+-- Created: 20 Feb 2019
+-- Domain: SQL
+-- Problem: https://www.hackerrank.com/challenges/select-by-id/problem
+-- Subdomain: Basic Select
+-- Title: Select by ID
+
+SELECT *
+FROM CITY
+WHERE ID = 1661
