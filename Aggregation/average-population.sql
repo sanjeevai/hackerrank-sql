547e88333dc72190face61ec74d1b785f1b1d3af
Sanjeev Yadav <sanjeev.yadav.ai@outlook.com>
2019-03-16 01:35:25 +0530

    feat: Add average population problem

---
+++ Aggregation/average-population.sql
@@ -0,0 +1,8 @@
+-- Author: Sanjeev Yadav
+-- Created: 15 Mar 2019
+-- Domain: SQL
+-- Problem: https://www.hackerrank.com/challenges/average-population/problem
+-- Subdomain: Aggregation
+-- Title: Average Population
+SELECT ROUND(AVG(POPULATION))
+FROM CITY
